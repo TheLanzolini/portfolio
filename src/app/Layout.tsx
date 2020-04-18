9a2ee5ca97808ab7b0b978f63193b651e7d38396
Alex Lanzoni <thelanzolini@gmail.com>
2020-04-18 03:37:53 -0700

    fix meta tags getting nullified

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -21,14 +21,18 @@ const Layout = (props: LayoutProps) => {
       <Helmet>
         {title && <title>{title}</title>}
         {description && <meta name="description" content={description} />}
-        <title>{title}</title>
-        <meta property="og:title" content={title} />
-        <meta name="twitter:title" content={title} />
-        <meta name="description" content={description} />
-        <meta name="twitter:description" content={description} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={imageUrl} />
-        <meta name="twitter:image" content={imageUrl} />
+        {title && <title>{title}</title>}
+        {title && <meta property="og:title" content={title} />}
+        {title && <meta name="twitter:title" content={title} />}
+        {description && <meta name="description" content={description} />}
+        {description && (
+          <meta name="twitter:description" content={description} />
+        )}
+        {description && (
+          <meta property="og:description" content={description} />
+        )}
+        {imageUrl && <meta property="og:image" content={imageUrl} />}
+        {imageUrl && <meta name="twitter:image" content={imageUrl} />}
       </Helmet>
       {props.children}
     </StyledLayout>
